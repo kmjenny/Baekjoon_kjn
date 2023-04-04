4a5de43415b47e083d952397b1a18ccc7dc1dff9
jenny <88226911+kmjenny@users.noreply.github.com>
2023-04-04 23:57:56 +0900

    new

---
+++ 24264.py
@@ -0,0 +1,6 @@
+# 알고리즘 수업 - 알고리즘의 수행 시간 3
+# 브론즈3
+import sys
+n = int(sys.stdin.readline())
+print(n**2)
+print("2")
---
+++ 24265.py
@@ -0,0 +1,9 @@
+# 알고리즘 수업 - 알고리즘의 수행 시간 4
+# 브론즈3
+import sys
+n = int(sys.stdin.readline())
+sum = 0
+for i in range(n):
+    sum += i
+print(sum)
+print("2")
